4610b0210cc5414947e7a8c1638adb8ad5f419cc
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-12-05 15:40:55 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace ClientReservasi_115
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.textBoxNoTlf = new System.Windows.Forms.TextBox();
+            this.textBoxJumlah = new System.Windows.Forms.TextBox();
+            this.textBoxIDLokasi = new System.Windows.Forms.TextBox();
+            this.btSimpan = new System.Windows.Forms.Button();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.btHapus = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.dtPemesanan = new System.Windows.Forms.DataGridView();
+            this.pemesananBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dtPemesanan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pemesananBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Reservasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "No Telfon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jumlah Pemesanan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ID Lokasi";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(190, 47);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(214, 20);
+            this.textBoxID.TabIndex = 5;
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(190, 84);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(214, 20);
+            this.textBoxNama.TabIndex = 6;
+            // 
+            // textBoxNoTlf
+            // 
+            this.textBoxNoTlf.Location = new System.Drawing.Point(190, 118);
+            this.textBoxNoTlf.Name = "textBoxNoTlf";
+            this.textBoxNoTlf.Size = new System.Drawing.Size(214, 20);
+            this.textBoxNoTlf.TabIndex = 7;
+            // 
+            // textBoxJumlah
+            // 
+            this.textBoxJumlah.Location = new System.Drawing.Point(190, 153);
+            this.textBoxJumlah.Name = "textBoxJumlah";
+            this.textBoxJumlah.Size = new System.Drawing.Size(214, 20);
+            this.textBoxJumlah.TabIndex = 8;
+            // 
+            // textBoxIDLokasi
+            // 
+            this.textBoxIDLokasi.Location = new System.Drawing.Point(190, 194);
+            this.textBoxIDLokasi.Name = "textBoxIDLokasi";
+            this.textBoxIDLokasi.Size = new System.Drawing.Size(214, 20);
+            this.textBoxIDLokasi.TabIndex = 9;
+            // 
+            // btSimpan
+            // 
+            this.btSimpan.Location = new System.Drawing.Point(539, 60);
+            this.btSimpan.Name = "btSimpan";
+            this.btSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btSimpan.TabIndex = 10;
+            this.btSimpan.Text = "Simpan";
+            this.btSimpan.UseVisualStyleBackColor = true;
+            this.btSimpan.Click += new System.EventHandler(this.btSimpan_Click);
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Location = new System.Drawing.Point(539, 89);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btUpdate.TabIndex = 11;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            this.btUpdate.Click += new System.EventHandler(this.btUpdate_Click);
+            // 
+            // btHapus
+            // 
+            this.btHapus.Location = new System.Drawing.Point(539, 143);
+            this.btHapus.Name = "btHapus";
+            this.btHapus.Size = new System.Drawing.Size(75, 23);
+            this.btHapus.TabIndex = 12;
+            this.btHapus.Text = "Hapus";
+            this.btHapus.UseVisualStyleBackColor = true;
+            this.btHapus.Click += new System.EventHandler(this.btHapus_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(539, 173);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(75, 23);
+            this.btClear.TabIndex = 13;
+            this.btClear.Text = "Clear";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // dtPemesanan
+            // 
+            this.dtPemesanan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtPemesanan.Location = new System.Drawing.Point(62, 259);
+            this.dtPemesanan.Name = "dtPemesanan";
+            this.dtPemesanan.Size = new System.Drawing.Size(668, 150);
+            this.dtPemesanan.TabIndex = 14;
+            this.dtPemesanan.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtPemesanan_CellContentClick);
+            // 
+            // pemesananBindingSource
+            // 
+            this.pemesananBindingSource.DataSource = typeof(ClientReservasi_115.ServiceReference1.Pemesanan);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtPemesanan);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btHapus);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.btSimpan);
+            this.Controls.Add(this.textBoxIDLokasi);
+            this.Controls.Add(this.textBoxJumlah);
+            this.Controls.Add(this.textBoxNoTlf);
+            this.Controls.Add(this.textBoxNama);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Reservasi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtPemesanan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pemesananBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.TextBox textBoxNoTlf;
+        private System.Windows.Forms.TextBox textBoxJumlah;
+        private System.Windows.Forms.TextBox textBoxIDLokasi;
+        private System.Windows.Forms.Button btSimpan;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Button btHapus;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.DataGridView dtPemesanan;
+        private System.Windows.Forms.BindingSource pemesananBindingSource;
+    }
+}
+
---
+++ IService1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi);//method proses input data
+
+        [OperationContract]
+        string editPemesanan(string IDPemesanan, string NamaCustomer, string No_telpon);
+
+        [OperationContract]
+        string deletePemesanan(string IDPemesanan);
+
+        [OperationContract]
+        List<CekLokasi> ReviewLokasi(); //nampilin data yang ada di database (select all) menampilkan isi dari yang ada contract
+
+        [OperationContract]
+        List<DetailLokasi> DetailLokasi();
+
+        [OperationContract]
+        List<Pemesanan> Pemesanan();
+        string GetData(int value);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    public class CekLokasi //daftar lokasi nongrong
+    {
+    
+        public string IDLokasi { get; set; } //variable dari public class
+
+   
+        public string NamaLokasi { get; set; }
+
+
+        public string DeskripsiSingkat { get; set; }
+    }
+
+
+    public class DetailLokasi //menampilkan detail lokasi
+    {
+      
+        public string IDLokasi { get; set; } //variable dari public class
+
+       
+        public string NamaLokasi { get; set; }
+
+        public string DeskripsiFull { get; set; }
+
+    
+        public int Kouta { get; set; }
+    }
+
+
+
+    public class Pemesanan //crate
+    {
+ 
+        public string IDPemesanan { get; set; } //variable dari public class
+
+
+        public string NamaCustomer { get; set; } // method
+
+    
+        public string NoTelepon { get; set; }
+
+
+        public int JumlahPemesanan { get; set; }
+
+
+        public string Lokasi { get; set; }
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceReservasi.ContractType".
+    /*[DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }*/
+}
